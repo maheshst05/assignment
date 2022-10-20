735c676c6d48378556da45c25d50d4569ef8a40d
tt-art <tawaletushar0@gmail.com>
2022-10-20 23:02:39 +0530

    palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,19 @@
-ith tu tuzi kasi ghal an mala ith string palindrome ahhe ka nahi te sabg bc..
+// ith tu tuzi kasi ghal an mala ith string palindrome ahhe ka nahi te sabg bc..
+
+let str="mada"
+let temp=""
+for(let i=str.length-1;i>=0;i--)
+{
+temp=temp+str[i]
+}
+if(temp==str)
+{
+    console.log("string is palindrome");
+}
+else{
+    console.log("string is not palindrome");
+    
+}
+
+
+
